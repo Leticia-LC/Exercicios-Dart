e24af8c2eb40618e717b12aee7353d485c384f0f
Leticia-LC <leticia.lc2005@aluno.ifsc.edu.br>
2024-03-22 11:00:57 -0300

    praticando

---
+++ Pratica.dart
@@ -0,0 +1,14 @@
+void main() {
+  late double resultado = _getValorTotal();
+  
+  resultado.toInt();
+  print("Chamando a funcao main");
+  
+  
+}
+
+double _getValorTotal(){
+  print('Chamando a func _getValorTotal');
+  
+  return 0.0;
+}
