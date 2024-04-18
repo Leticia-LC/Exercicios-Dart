f5dc01fd3c05c03a9d3d27cd0364821ee2ed6028
Leticia-LC <leticia.lc2005@aluno.ifsc.edu.br>
2024-04-18 10:13:30 -0300

    .

---
+++ flutter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+  
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Olá Mundo',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                   ),
+      ), // Text
+    ), // Center
+  ); // MaterialApp
+}
